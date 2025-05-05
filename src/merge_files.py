384505b0ea9ff0e2da83d253daa32e740c475fde
Mihika Rao <xsw5kn@virginia.edu>
2025-05-05 15:05:58 -0400

    changes

--- src/merge_files.py
+++ src/merge_files.py
@@ -1,11 +1,8 @@
 import pandas as pd
 
-# Load both CSVs
 df1 = pd.read_csv('data/processed/features.csv')
 df2 = pd.read_csv('data/processed/features1.csv')
 
-# Concatenate them vertically
 merged = pd.concat([df1, df2], ignore_index=True)
 
-# Save to a new CSV
 merged.to_csv('data/processed/features_combined.csv', index=False)
--- src/read_pcap.py
+++ src/read_pcap.py
@@ -2,7 +2,6 @@ import pyshark
 
 cap = pyshark.FileCapture('data/raw/cnn/cnn_1.pcap')
 
-# Loop through the first 5 packets
 for i, pkt in enumerate(cap):
     if i >= 5:
         break
